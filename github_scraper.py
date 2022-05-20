44104186eb527cbd7b26ffee2a44264c6d6bc1eb
Oliver McManus <ollie@Olivers-MacBook-Pro.local>
2022-05-20 21:39:22 +0100

    renamed web_scraper to github_scraper removed one function from github_scraper

--- github_scraper.py
+++ github_scraper.py
@@ -12,9 +12,5 @@ print(f'Profile Image Link: {profile_image}')
 full_name = soup.find('span', {'itemprop' : 'name'}).text.strip()
 print(f'Full Name: {full_name}')
 
-#FIX THIS!!
-#repo_number = soup.find('a', {'data-tab-item' : 'repositiories'})
-#print(f'Number Of Repositories: {repo_number}')
-
 location = soup.find('span', {'class' :'p-label'}).text
 print(f'And They Live In: {location}')
